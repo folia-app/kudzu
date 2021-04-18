e0274e52b3418f7bae8e68d6df9770735ff22f68
billy rennekamp <billy.rennekamp@gmail.com>
2021-04-18 15:56:34 +0200

    tweaks, added event

--- contracts/Kudzu.sol
+++ contracts/Kudzu.sol
@@ -10,17 +10,23 @@ import "./Metadata.sol";
 
 contract Kudzu is ERC721, Ownable {
     Metadata public metadata;
+
+    event Infect(address indexed from, address indexed to, uint256 indexed tokenId);
+
     constructor(Metadata _metadata) public ERC721("FoliaVirus", "FLV") {
         metadata = _metadata;
         uint256 tokenId = 1;
 
         tokenId = tokenId << 8;
-        tokenId = tokenId | pseudoRNG(32);
+        tokenId = tokenId | pseudoRNG(32, 1);
 
         tokenId = tokenId << 8;
-        tokenId = tokenId | pseudoRNG(32);
-
+        tokenId = tokenId | pseudoRNG(32, 2);
         _mint(msg.sender, tokenId);
+        emit Infect(address(0), msg.sender, tokenId);
+    }
+    function getPiecesOfTokenID(uint256 tokenId) public pure returns(uint256 id, uint256 eyes, uint256 mouth) {
+        return (tokenId >> 16, (tokenId >> 8 & 0xFF), tokenId & 0xFF);
     }
     function updateMetadata(Metadata _metadata) public onlyOwner {
         metadata = _metadata;
@@ -29,23 +35,24 @@ contract Kudzu is ERC721, Ownable {
         require(balanceOf(from) == 1, "NOT YET INFECTED");
         require(balanceOf(to) == 0, "ALREADY INFECTED");
         uint256 tokenId = totalSupply() + 1;
-        tokenId = tokenId << 4;
-        if (pseudoRNG(2) == 0) {
+        tokenId = tokenId << 8;
+        if (pseudoRNG(2, 1) == 0) {
             //inherit eyes
-            tokenId = tokenId | (parentId >> 4 & 0xFF);
+            tokenId = tokenId | (parentId >> 8 & 0xFF);
             tokenId = tokenId << 8;
-            tokenId = tokenId | pseudoRNG(32);
+            tokenId = tokenId | pseudoRNG(32, 2);
         } else {
             //inherit mouth
-            tokenId = tokenId | pseudoRNG(32);
+            tokenId = tokenId | pseudoRNG(32, 3);
             tokenId = tokenId << 8;
             tokenId = tokenId | (parentId & 0xFF);
         }
         _mint(to, tokenId);
+        emit Infect(from, to, tokenId);
     }
     function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override {}
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public virtual override {}
-    function pseudoRNG(uint modulo) private view returns (uint256) {
-        return uint256(keccak256(abi.encodePacked(block.difficulty, now))) % modulo;
+    function pseudoRNG(uint modulo, uint nonce) private view returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(block.difficulty, now, totalSupply(), nonce))) % modulo;
     }
 }
--- test/Kudzu.test.js
+++ test/Kudzu.test.js
@@ -1,23 +1,32 @@
-var Sample = artifacts.require('./Sample.sol')
+var Metadata = artifacts.require('./Metadata.sol')
+var Kudzu = artifacts.require('./Kudzu.sol')
 var BigNumber = require('bignumber.js')
 let gasPrice = 1000000000 // 1GWEI
 
 let _ = '        '
 
-contract('Sample', async function(accounts) {
-  let sample
+contract('Kudzu', async function(accounts) {
+  let kudzu, metadata
 
   before(done => {
     ;(async () => {
       try {
         var totalGas = new BigNumber(0)
 
-        // Deploy Sample.sol
-        sample = await Sample.new()
-        var tx = await web3.eth.getTransactionReceipt(sample.transactionHash)
+        // Deploy Metadata.sol
+        metadata = await Metadata.new()
+        var tx = await web3.eth.getTransactionReceipt(metadata.transactionHash)
         totalGas = totalGas.plus(tx.gasUsed)
-        console.log(_ + tx.gasUsed + ' - Deploy sample')
-        sample = await Sample.deployed()
+        console.log(_ + tx.gasUsed + ' - Deploy metadata')
+        metadata = await Metadata.deployed()
+
+
+        // Deploy Kudzu.sol
+        kudzu = await Kudzu.new(metadata.address)
+        var tx = await web3.eth.getTransactionReceipt(kudzu.transactionHash)
+        totalGas = totalGas.plus(tx.gasUsed)
+        console.log(_ + tx.gasUsed + ' - Deploy kudzu')
+        kudzu = await Kudzu.deployed()
 
         console.log(_ + '-----------------------')
         console.log(_ + totalGas.toFormat(0) + ' - Total Gas')
@@ -29,10 +38,29 @@ contract('Sample', async function(accounts) {
     })()
   })
 
-  describe('Sample.sol', function() {
+  describe('Kudzu.sol', function() {
     it('should pass', async function() {
+
+
+      for (var i = 0; i < 8; i++) {
+        var tokenId = await kudzu.tokenByIndex(i)
+        console.log({tokenId: tokenId.toString(10), tokenId16: tokenId.toString(16)})
+  
+        var parts = await kudzu.getPiecesOfTokenID(tokenId)
+        console.log({
+          parts0: parts[0].toString(10),
+          parts016: parts[0].toString(16),
+          parts1: parts[1].toString(10),
+          parts2: parts[2].toString(10),
+        })
+  
+        await kudzu.transferFrom(accounts[i], accounts[i + 1], tokenId)
+  
+      }
+
+
       assert(
-        true === true,
+        true === false,
         'this is true'
       )
     })
