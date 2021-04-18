16990bf09192a724433d8a1420ebca43cf9b9670
billy rennekamp <billy.rennekamp@gmail.com>
2021-04-18 12:04:23 +0200

    about to remove all metadata info

--- contracts/Kudzu.sol
+++ contracts/Kudzu.sol
@@ -12,16 +12,40 @@ contract Kudzu is ERC721, Ownable {
     Metadata public metadata;
     constructor(Metadata _metadata) public ERC721("FoliaVirus", "FLV") {
         metadata = _metadata;
-        _mint(msg.sender, 1);
+        uint256 tokenId = 1;
+
+        tokenId = tokenId << 8;
+        tokenId = tokenId | pseudoRNG(32);
+
+        tokenId = tokenId << 8;
+        tokenId = tokenId | pseudoRNG(32);
+
+        _mint(msg.sender, tokenId);
     }
     function updateMetadata(Metadata _metadata) public onlyOwner {
         metadata = _metadata;
     }
-    function transferFrom(address from, address to, uint256 tokenId) public virtual override {
+    function transferFrom(address from, address to, uint256 parentId) public virtual override {
         require(balanceOf(from) == 1, "NOT YET INFECTED");
         require(balanceOf(to) == 0, "ALREADY INFECTED");
-        _mint(to, totalSupply() + 1);
+        uint256 tokenId = totalSupply() + 1;
+        tokenId = tokenId << 4;
+        if (pseudoRNG(2) == 0) {
+            //inherit eyes
+            tokenId = tokenId | (parentId >> 4 & 0xFF);
+            tokenId = tokenId << 8;
+            tokenId = tokenId | pseudoRNG(32);
+        } else {
+            //inherit mouth
+            tokenId = tokenId | pseudoRNG(32);
+            tokenId = tokenId << 8;
+            tokenId = tokenId | (parentId & 0xFF);
+        }
+        _mint(to, tokenId);
     }
     function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override {}
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public virtual override {}
+    function pseudoRNG(uint modulo) private view returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(block.difficulty, now))) % modulo;
+    }
 }
--- metadata/index.js
+++ metadata/index.js
@@ -1,22 +1,14 @@
-var canvas = document.getElementById("myCanvas");
-console.log({canvas})
 
+var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var size = 512
+var size = 768
 canvas.width = size
 canvas.height = size
 let rows, margin, circleSize, radius, totalRows, offset
 // ctx.fillRect(0, 0, size, size);
 // var circleSize = size / (rows + margin * 2)
 
-
-  
-function makeLeaf() {
-
-  let choose = (Math.floor(Math.random() * 4) + 1  )
-  makeLeafPointing(choose)
-}
-
+// Math.seedrandom('asfdasf');
 
 
 var _5x5 = [
@@ -76,27 +68,60 @@ var _8x8 = [
   [1,1,1,0,0,0,0,0,0,0,0,0,1,1,1],
   [1,1,1,1,0,0,0,0,0,0,0,1,1,1,1],
   [1,1,1,1,0,0,0,0,0,0,0,1,1,1,1],
-  [1,1,1,1,1,1,0,0,0,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,1,1,3,3,1,0,0,0,1,1,1,1,1,1],
+  [1,1,1,3,1,1,1,1,1,1,1,1,1,1,1],
   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 ]
 
-function makeLeafPointing(choose) {
-
-
+async function makeLeaf() {
   rows = _8x8.length
   margin = 1
   circleSize = size / (rows + margin * 2)
-  radius = circleSize / 2 
+  radius = circleSize / 1.25 
   totalRows = size / circleSize
   offset =  margin * circleSize
-  makeBase(_8x8)
-  makePoint(_8x8)
+
+  var [grid, corner, rotation] = getGrid(_8x8)
+  // makeBG(corner, rotation)
+  // await addStem()
+  // makeStem(grid)
+  makeAll(grid)
+  addPNG()
+  // makeEyes()
 }
 
+function addStem() {
+  return new Promise((resolve, reject) => {
+    var drawing2 = new Image();
+    drawing2.src = `stem-4.png`;
+    drawing2.onload = function() {
+      ctx.drawImage(drawing2,0,0);
+      resolve()
+    };
+  })
+}
 
-function makePoint(grid) {
+function addPNG() {
+  var drawing = new Image();
+  var name = Math.floor(Math.random() * 34) + 1
+  // name = 5
+  var drawingSize = 480;
+  var drawingPos = (size - drawingSize) / 2
+  drawing.src = `/eyes/${name}.png`;
+  drawing.onload = function() {
+    ctx.drawImage(drawing,drawingPos,drawingPos);
+  };
+
+  var drawing2 = new Image();
+  var name2 = Math.floor(Math.random() * 34) + 1
+  drawing2.src = `/mouth/${name2}.png`;
+  drawing2.onload = function() {
+    ctx.drawImage(drawing2,drawingPos,drawingPos);
+  };
+}
+
+function getGrid(grid) {
   var corner = Math.floor(Math.random() * 4) + 1
   var rotation = Math.floor(Math.random() * 2) + 1
   // corner = 4;
@@ -123,52 +148,59 @@ function makePoint(grid) {
       grid.map(r => r.reverse())
       break;
   }
-
-  for (var i = 0; i < grid.length; i++) {
-    var row = grid[i]
-    for (var j = 0; j < row.length; j++) {
-      var fill = grid[i][j] == 8
-      if (!fill) continue
-      makeCircle(i, j)
-    }
-  }
+  return [grid, corner, rotation]
 }
 
-function makeBase(grid) {
+
+
+function makeBG(corner, rotation) {
+  var radgrad = ctx.createRadialGradient(size/2,size/2 , 0, size/2, size/2, size/2 );
+  radgrad.addColorStop(0, 'rgba(0,0,0,0.5)');
+  radgrad.addColorStop(1, 'rgba(0,0,0,0)');
+  ctx.fillStyle = radgrad;
+  ctx.fillRect(0,0,size,size);
+}
+
+function makeStem(grid) {
   for (var i = 0; i < grid.length; i++) {
-    var row = grid[i]
-    for (var j = 0; j < row.length; j++) {
-      var fill = grid[i][j] == 0
-      if (fill) {
-        makeCircle(i, j)
-      } else if (grid[i][j] == 3){
-        makeCircle(i, j, '#F00')
+    for (var j = 0; j < grid.length; j++) {
+      if (grid[i][j] == 3) {
+        makeSmallLeaf(i,j, '#8B4513')
       }
     }
   }
 }
+function makeAll(grid) {
 
-
-function getGreen() {
-  return `#00${(Math.floor(Math.random() * 256) + 1  ).toString(16)}00`
+  var sequence = Array.from(Array(grid.length * grid.length).keys())
+  shuffle(sequence)
+  for (var i = 0; i < sequence.length; i++) {
+    var row = Math.floor(sequence[i] / grid.length)
+    var col = (sequence[i] % grid.length) 
+    if (grid[row][col] == 0 || grid[row][col] == 8) {
+      // if (grid[row][col] == 0) {
+        makeSmallLeaf(row, col)
+    }
+  }
 }
 
-function makeCircle (i,j, color) {
+function getGreen() {
+  if (Math.random() > .92) {
+    // return '#663399'
+    // return `#00${(Math.floor(Math.random() * 100) + 101  ).toString(16) + (Math.floor(Math.random() * 100) + 101  ).toString(16)}`
+  }
+  if (Math.random() > .92) {
+    // return '#FFD700'
+    // return `#00${(Math.floor(Math.random() * 100) + 101  ).toString(16) + (Math.floor(Math.random() * 100) + 101  ).toString(16)}`
+  }
+  return `#00${(Math.floor(Math.random() * 100) + 101  ).toString(16)}00`
+}
+
+function makeSmallLeaf (i,j, color) {
   var x =  (i * circleSize) + (circleSize / 2) + offset;
   var y =  (j * circleSize) + (circleSize / 2) + offset;
-  ctx.beginPath();
-  var startAngle = 0
-  var endAngle = 2 * Math.PI
-  var counterclockwise = 0
-  ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise);
-  // ctx.arc(x, y, radius * 1.5, 0, .5 * Math.PI, 0)
-
   ctx.fillStyle = color ||getGreen();
-  ctx.fill(); 
-
   let rotate 
-
-
   ctx.beginPath();
   var side = Math.floor(Math.random() * 4) + 1
 
@@ -194,30 +226,15 @@ function makeCircle (i,j, color) {
       ctx.arc(x + radius*2 , y - (radius * .1), radius* 2, rotate, rotate, 0)
       break;
   }
-  // ctx.fillStyle = '#ff0000'
+
+  // circle
+  var startAngle = 0
+  var endAngle = 2 * Math.PI
+  var counterclockwise = 0
+  ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise);
   ctx.fill(); 
 
-
 }
-
-// ctx.beginPath();
-// foo = 256 / 2
-// r = foo / 2
-// ctx.arc(foo, foo, r, 0, 2 * Math.PI, 0);
-// ctx.fill(); 
-
-// ctx.beginPath();
-// ctx.arc(foo / 2, foo, r * 2, 0, .4 * Math.PI, 0);
-// // ctx.drawLine(0,0)
-// ctx.arc(0, foo, r* 2, 0, 0, 0)
-// ctx.fill(); 
-
-
-
-makeLeaf()
-
-makeEyes()
-
 function makeEyes () {
 
    var y = size / 2
@@ -262,6 +279,94 @@ function makeEyes () {
   ctx.fill(); 
 }
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+function makeSingleLeaf(x, y, clockwise, direction, color) {
+  // ctx.fillStyle = '#bbb'
+  ctx.fillStyle = "rgba(100 ,100, 100, 1)";
+  // ctx.filter = `blur(${size / 55}px)`;
+
+  // ctx.shadowColor = "black";
+  // ctx.shadowBlur = 6;
+  // ctx.shadowOffsetX = 0;
+  // ctx.shadowOffsetY = 0;
+  ctx.beginPath();
+  width = size / 4
+  x = size / 2
+  y = size / 2
+  corner = 1
+  rotation = 1
+  console.log({corner, rotation})
+  ctx.arc(x, y, width, 0, 2 * Math.PI, 0);
+  // ctx.fill(); 
+  if (rotation == 2) {
+    if (corner == 1) {
+
+    } else if (corner == 2) {
+
+    }else if (corner == 3) {
+
+    }else if (corner == 4) {
+
+    }
+  } else {
+    if (corner == 1) {
+      var xx = x - width
+      var yy = y
+      var wwidth = width * 2
+      ctx.arc(xx, yy, wwidth, 0, .4 * Math.PI, 0)
+
+      var xxx = x * .7
+      var yyy = y + width * 1.4
+      var wwwidth = width / 1.8
+      ctx.arc(xxx, yyy, wwwidth, .333 * Math.PI,  1.6 * Math.PI, 1)
+
+      // ctx.lineTo(x, y);
+    } else if (corner == 2) {
+
+    }else if (corner == 3) {
+
+    }else if (corner == 4) {
+
+    }
+  }
+
+  // ctx.beginPath();
+
+  ctx.shadowBlur = 10;
+
+  // ctx.shadowBlur = 1;
+
+ctx.shadowOffsetX = 0;
+ctx.shadowOffsetY = 0;
+  // ctx.arc(0, size/2, size/2, 0, 0, 0, 0)
+  ctx.fill(); 
+    // ctx.save(); // saves the coordinate system
+  ctx.rotate(45); // <<< set current transformation to rotated
+  // ctx.closePath();
+
+  ctx.shadowBlur = 0;
+  ctx.filter = 'blur(0px)';
+}
+
 // var happy = Math.random() > .5
 // // happy = false
 
@@ -301,3 +406,6 @@ function makeEyes () {
 // 28116F68
 // 72440349
 // 61545D91
+
+
+  makeLeaf()
